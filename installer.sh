62b9386ee4f7e9ac5187be3064007897d2755050
jjchoNC <5025221001@mhs.its.ac.id>
2024-05-17 01:17:43 +0700

    fix: fix installer.sh

--- installer.sh
+++ installer.sh
@@ -3,8 +3,7 @@ until docker exec mysql mysqladmin ping -h "localhost" --silent; do
     sleep 1
 done
 
+docker exec -i php php artisan config:clear
+docker exec -i php php artisan storage:link
+docker exec -i php php artisan key:generate
 docker exec -i php php artisan migrate --seed
-php artisan config:clear
-php artisan storage:link
-php artisan key:generate
-php artisan migrate --seed
